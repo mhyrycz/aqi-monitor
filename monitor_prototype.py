c286fffaae370079a732ef8696d6919e434abe76
Maciej Hyrycz <maciek.hyrycz@gmail.com>
2018-11-08 19:07:17 +0100

    implemented improvements, splitted script for methods

--- monitor_prototype.py
+++ monitor_prototype.py
@@ -1,20 +1,28 @@
 import miio
+from miio.airpurifier import AirPurifier, OperationMode
 import time
 import logging
 from influxdb import InfluxDBClient
+#from pudb import set_trace; set_trace()
 
-def write_data(ip, token, series):
-    logging.basicConfig(filename='example.log',level=logging.DEBUG)
-    device = miio.airqualitymonitor.AirQualityMonitor(ip=f'{ip}',
-                                                      token=f'{token}',
-                                                      start_id=0,
-                                                      debug=0,
-                                                      lazy_discover=True)
+def establish_connection(xiaomi_type, ip, token):
+    device = xiaomi_type(ip=f'{ip}',
+                token=f'{token}',
+                start_id=0,
+                debug=0,
+                lazy_discover=True)
+    return device
 
-    device.on()
-    logging.info('Turned on')
-    time.sleep(20)
-    air_status = device.status().aqi
+
+def write_data(device_connected, series):
+    if device_connected.status().power == "off":
+        device_connected.on()
+        time.sleep(20)
+        was_turned_on = False
+    else:
+        was_turned_on = True
+    #import pudb; pu.db
+    air_status = device_connected.status().aqi
     timestamp = [
         {
             "measurement": f"{series}",
@@ -25,8 +33,8 @@ def write_data(ip, token, series):
     ]
     client = InfluxDBClient('localhost', 8086, 'root', 'root', 'xiaomi')
     written_data = client.write_points(timestamp)
-    logging.info(f'{written_data}')
-    device.off()
+    if was_turned_on == False:
+        device_connected.off()
 
-write_data('192.168.0.101', 'e1ad2a2d4343fedd0c558ff49d261510', 'pm25')
-write_data('192.168.0.206', '7aaf72414d8606bff08feeb655afe0fb', 'purifier')
+write_data(establish_connection(miio.airqualitymonitor.AirQualityMonitor, '192.168.0.101', 'e1ad2a2d4343fedd0c558ff49d261510'),'pm25')
+write_data(establish_connection(miio.airpurifier.AirPurifier, '192.168.0.206', '7aaf72414d8606bff08feeb655afe0fb'),'purifier')
